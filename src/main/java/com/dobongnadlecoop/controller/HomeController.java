197ee6b6eb360d71cecd8c840ae9850a372e3db5
NamMinji <73888823+rhdiddl9911@users.noreply.github.com>
2021-02-14 03:49:31 +0900

    메인화면 공지 불러오기 컨트롤러 동작 추가

--- src/main/java/com/dobongnadlecoop/controller/HomeController.java
+++ src/main/java/com/dobongnadlecoop/controller/HomeController.java
@@ -1,15 +1,29 @@
 package com.dobongnadlecoop.controller;
 
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.dobongnadlecoop.service.NoticeBoardService;
+import com.dobongnadlecoop.utils.PageRequest;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 public class HomeController {
-
+	
+	private final NoticeBoardService noticeService;
+	
 	@GetMapping("/")
 	public ModelAndView HomeReturn() {
 		ModelAndView model = new ModelAndView("index");
+		
+		PageRequest pageable = new PageRequest(1, 5, Direction.DESC);
+		
+		model.addObject("noticeBoardList", noticeService.getTitleList(pageable.of()).getContent());
+		
 		return model;
 	}
 	
--- src/main/webapp/WEB-INF/JSP/index.jsp
+++ src/main/webapp/WEB-INF/JSP/index.jsp
@@ -34,10 +34,19 @@
 				<th>작성일</th>
 			</tr>
 
-			<c:if test="${noticeBoardData == null}">
+			<c:if test="${noticeBoardList.size() == 0}">
 				<tr>
 					<td colspan="2" id="nulldata">아직 데이터가 없습니다.</td>
 				</tr>
+			</c:if>
+			
+			<c:if test="${noticeBoardList.size() > 0}">
+				<c:forEach var="boardData" items="${noticeBoardList}">
+					<tr onclick="location.href='notice/${boardData.seq}?page=1'" class="boarditem">
+						<td>${boardData.title}</td>
+						<td>${boardData.createdate}</td>
+					</tr>
+				</c:forEach>
 			</c:if>
 
 		</table>
